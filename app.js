f254d89ec8afff11e76c3fa7449359223a37fa83
Cole <colegelinas13@gmail.com>
2025-06-25 14:45:56 -0500

    Sprint 12 Final Pre Vid 9

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index.js");
+const userRouter = require("./routes/users.js");
 
 const app = express();
 const { PORT = 3001 } = process.env;
